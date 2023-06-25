b6fe5e2b01dbfdb95119318b70f875ceecfc1e50
KZ <kjreem.imagine@gmail.com>
2023-06-25 23:42:38 +0330

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context){
+      return MaterialApp(
+        title: 'Weather App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+            iconTheme: IconThemeData(
+              color: Colors.blue
+            ),
+            elevation: 0,
+          ),
+          scaffoldBackgroundColor: Colors.white,
+          primaryColor: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.white),
+        ),
+      );
+  }
+}
